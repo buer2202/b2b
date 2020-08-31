f2809c8f96c518ac03e88b30f38574e921c355c7
Buer Huang <buer2202@163.com>
2020-08-31 10:51:42 +0800

    优化aes加密，增加填充方式参数。

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Goods;
 use App\Models\User;
 use App\Repositories\Home\PriceGroupGoodsRepository;
+use App\Services\Aes256cbc;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index(Request $request)
     {
-        // 通用用户商品列表获取方法
+        #
+    }
+
+    // 通用用户商品列表获取方法
+    public function getGoodsList()
+    {
         $dataList = PriceGroupGoodsRepository::goodsList(Goods::class);
         dump($dataList);
     }
--- app/Services/Aes265cbc.php
+++ app/Services/Aes265cbc.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\Exceptions\CustomException;
@@ -11,15 +12,24 @@ class Aes256cbc
 {
     private $key;
     private $cipher;
+    private $options;
 
     /**
      * Aes constructor.
      * @param $key
      */
-    function __construct($key, $cipher = 'aes-256-cbc')
+    function __construct($key, $cipher = 'aes-256-cbc', $options = OPENSSL_RAW_DATA)
     {
-        $this->key    = $key;
-        $this->cipher = $cipher;
+        $this->key     = $key;
+        $this->cipher  = $cipher;
+
+        /**
+         * 填充方式
+         * OPENSSL_RAW_DATA 或 1：用PKCS#7填充
+         * OPENSSL_ZERO_PADDING 或 2：用\0填充（有bug）
+         * OPENSSL_NO_PADDING 或 3：不填充
+         */
+        $this->options = $options;
     }
 
     /**
@@ -32,9 +42,16 @@ class Aes256cbc
             $value = json_encode($value);
         }
 
+        // 如果是0填充，则手动填充\0。（自动填充有问题）
+        if ($this->options == OPENSSL_ZERO_PADDING) {
+            $this->options = OPENSSL_NO_PADDING;
+            $value = $this->padZero($value);
+        }
+
         $iv = str_random(openssl_cipher_iv_length($this->cipher));
-        // OPENSSL_RAW_DATA方式【会用PKCS#7进行补位】
-        $value = openssl_encrypt($value, $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv);
+
+        // 加密
+        $value = openssl_encrypt($value, $this->cipher, $this->key, $this->options, $iv);
         if ($value === false) {
             throw new CustomException('加密失败');
         }
@@ -42,6 +59,26 @@ class Aes256cbc
         $json = json_encode(['iv' => base64_encode($iv), 'value' => base64_encode($value)]);
 
         return base64_encode($json);
+    }
+
+    // 增加\0填充
+    private function padZero($string)
+    {
+        // AES的区块长度固定为128比特，即16字节
+        $blockSize = 16;
+
+        // 获取待加密的字符串长度
+        $len = strlen($string);
+
+        // 计算填充长度（与16整数倍的长度差）
+        $padLength = $blockSize - ($len % $blockSize);
+
+        // 如果有长度差，则填充 \0
+        if ($padLength) {
+            $string .= str_repeat("\0", $padLength);
+        }
+
+        return $string;
     }
 
     /**
@@ -60,11 +97,26 @@ class Aes256cbc
             throw new CustomException('解密参数失败');
         }
 
+        // 如果是0填充，则手动操作。（自动有问题）
+        if ($this->options == OPENSSL_ZERO_PADDING) {
+            $options = OPENSSL_NO_PADDING;
+        } else {
+            $options = $this->options;
+        }
+
         $iv = base64_decode($obj->iv);
         $value = base64_decode($obj->value);
-        $decrypted = openssl_decrypt($value, $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv);
+
+        // 解密
+        $decrypted = openssl_decrypt($value, $this->cipher, $this->key, $options, $iv);
         if ($decrypted === false) {
             throw new CustomException('解密失败');
+        }
+
+        // 如果是0填充，则手动去除\0
+        if ($this->options == OPENSSL_ZERO_PADDING) {
+            // 去除\0填充
+            $decrypted = rtrim($decrypted, "\0");
         }
 
         if ($jsonDecode) {
